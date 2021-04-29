5ac3ae3ca7581e4567519fff3c287612b08e2499
Marco <marcosolina@gmail.com>
2021-04-29 18:57:34 +0000

    Check events script

---
+++ Linux/checkEvent.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+OLD_VALUE="NO"
+
+CSGO_INSTALL_FOLDER_FOLDER=$ENV_CSGO_INSTALL_FOLDER
+EVENT_FILE=$CSGO_INSTALL_FOLDER/csgo/addons/sourcemod/event.txt
+
+echo ""
+echo "Copying the demo files"
+echo ""
+DATE=$(date +'%Y-%m-%d')
+FOLDER_DEM="$ENV_SSH_FOLDER/demfiles/$DATE"
+
+
+while :
+do
+	NEW_VALUE=$(tr -d '\n' < $FILE_PATH)
+	if [ "$OLD_VALUE" = "$NEW_VALUE" ]; then
+		echo "equal"
+	else
+		OLD_VALUE=$NEW_VALUE
+		if [ "$NEW_VALUE" = "End Map" ]; then
+                     sleep 30
+		     echo "Copying dem files"
+		     ssh $ENV_SSH_USER@$ENV_SSH_IP mkdir -p $FOLDER_DEM
+                     scp $CSGO_INSTALL_FOLDER_FOLDER/csgo/*.dem $ENV_SSH_USER@$ENV_SSH_IP:$FOLDER_DEM
+                     curl --location --request POST 'https://marco.selfip.net/demparser/newdata' --header 'Content-Type: application/json' --data-raw '{"forceDeleteBadFiles": true}'
+		fi
+		echo "not"
+	fi
+	sleep 1
+done
+
