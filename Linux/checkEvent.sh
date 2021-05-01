592041a566c4398a9ad511bb385be90cd7cad65b
Marco Solina <marcosolina@gmail.com>
2021-05-01 17:46:52 +0200

    Update checkEvent.sh

--- Linux/checkEvent.sh
+++ Linux/checkEvent.sh
@@ -21,13 +21,14 @@ do
                 OLD_VALUE="NO"
                 echo "sending event $NEW_VALUE"
                 echo "NO" > $EVENT_FILE
-                curl --location --request POST 'https://marco.selfip.net/rcon/event' --header 'Content-Type: application/json' --data-raw "{\"eventName\": \"$NEW_VALUE\"}"
-                if [ "$NEW_VALUE" = "End Map"]; then
+                curl --location --request POST "http://$ENV_SSH_IP:8087/rcon/event" --header 'Content-Type: application/json' --data-raw "{\"eventName\": \"$NEW_VALUE\"}"
+                if [ "$NEW_VALUE" = "End Map" ]; then
                         sleep 35
                         DATE=$(date +'%Y-%m-%d')
                         FOLDER_DEM="$ENV_SSH_FOLDER/demfiles/$DATE"
                         ssh $ENV_SSH_USER@$ENV_SSH_IP mkdir -p $FOLDER_DEM
                         scp $CSGO_INSTALL_FOLDER_FOLDER/csgo/*.dem $ENV_SSH_USER@$ENV_SSH_IP:$FOLDER_DEM
+                        curl --location --request POST 'http://$ENV_SSH_IP:8080/demparser/newdata' --header 'Content-Type: application/json' --data-raw '{"forceDeleteBadFiles": true}'
                 fi
         fi
 
