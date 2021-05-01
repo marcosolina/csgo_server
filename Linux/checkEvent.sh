fb78b8c21b051b25ec350c9527aac8445201341c
Marco Solina <marcosolina@gmail.com>
2021-05-01 17:23:57 +0200

    Update checkEvent.sh

--- Linux/checkEvent.sh
+++ Linux/checkEvent.sh
@@ -5,26 +5,30 @@ OLD_VALUE="NO"
 CSGO_INSTALL_FOLDER_FOLDER=$ENV_CSGO_INSTALL_FOLDER
 EVENT_FILE=$ENV_CSGO_INSTALL_FOLDER/csgo/addons/sourcemod/event.txt
 
+echo "NO" > $EVENT_FILE
+
 DATE=$(date +'%Y-%m-%d')
 FOLDER_DEM="$ENV_SSH_FOLDER/demfiles/$DATE"
 
 
 while :
 do
-	NEW_VALUE=$(tr -d '\n' < $EVENT_FILE)
-	if [ "$OLD_VALUE" = "$NEW_VALUE" ]; then
-		echo "equal"
-	else
-		OLD_VALUE=$NEW_VALUE
-		if [ "$NEW_VALUE" = "End Map" ]; then
-                     sleep 40
-		     echo "Copying dem files"
-		     ssh $ENV_SSH_USER@$ENV_SSH_IP mkdir -p $FOLDER_DEM
-                     scp $CSGO_INSTALL_FOLDER_FOLDER/csgo/*.dem $ENV_SSH_USER@$ENV_SSH_IP:$FOLDER_DEM
-                     curl --location --request POST 'https://marco.selfip.net/demparser/newdata' --header 'Content-Type: application/json' --data-raw '{"forceDeleteBadFiles": true}'
-		fi
-		echo "not"
-	fi
-	sleep 1
-done
+        NEW_VALUE=$(tr -d '\n' < $EVENT_FILE)
+        if [ "$OLD_VALUE" = "$NEW_VALUE" ]; then
+                # echo "equal"
+        else
+                OLD_VALUE="NO"
+                echo "sending event $NEW_VALUE"
+                echo "NO" > $EVENT_FILE
+                curl --location --request POST 'https://marco.selfip.net/rcon/event' --header 'Content-Type: application/json' --data-raw "{\"eventName\": \"$NEW_VALUE\"}"
+                if [ "$NEW_VALUE" = "End Map"]; then
+                        sleep 35
+                        DATE=$(date +'%Y-%m-%d')
+                        FOLDER_DEM="$ENV_SSH_FOLDER/demfiles/$DATE"
+                        ssh $ENV_SSH_USER@$ENV_SSH_IP mkdir -p $FOLDER_DEM
+                        scp $CSGO_INSTALL_FOLDER_FOLDER/csgo/*.dem $ENV_SSH_USER@$ENV_SSH_IP:$FOLDER_DEM
+                fi
+        fi
 
+        sleep 1
+done
