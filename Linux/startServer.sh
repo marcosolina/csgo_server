2d801208e96ac48e7efc88d6f65024a991bcaa3c
Marco Solina <marcosolina@gmail.com>
2023-11-30 22:21:37 +0100

    test

--- Linux/startServer.sh
+++ Linux/startServer.sh
@@ -7,7 +7,7 @@ CS2_DIR="${HOMEDIR}/cs2"
 STEAMCMDDIR="${HOMEDIR}/steamcmd"
 
 ${STEAMCMDDIR}/steamcmd.sh +force_install_dir "${CS2_DIR}" \
-                                +login \
+                                +login gnari \
                                 +app_update "${STEAMAPPID}" \
                                 +quit
 
